ee4ddb713d4ef65943d3e1e85521d42a3108e712
Authentic Design <s.benning66@gmail.com>
2017-12-19 18:27:03 +0100

    finally stable

--- src/providers/camera/camera.ts
+++ src/providers/camera/camera.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the CameraProvider provider.
@@ -11,26 +13,41 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 @Injectable()
 export class CameraProvider {
 
-  pictures: any[] = [];
-  options: CameraOptions;
+  options: CameraOptions = <CameraOptions>{
+    quality: 100,
+    targetWidth: 900,
+    targetHeight: 600,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    saveToPhotoAlbum: false,
+    allowEdit: true,
+    sourceType: 1
+  };
+
+  private pictures: string[] = [];
+  private errors: string[] = [];
+
+  private _pictures$: BehaviorSubject<string[]> = new BehaviorSubject(this.pictures);
+  pictures$: Observable<string[]> = this._pictures$.asObservable();
+
+  private _cameraError$: BehaviorSubject<string[]> = new BehaviorSubject(this.errors);
+  cameraError$: Observable<string[]> = this._cameraError$.asObservable();
   
   constructor(public camera: Camera) {
     console.log('Hello CameraProvider Provider');
-    this.options = <CameraOptions>{
-      quality: 100,
-      targetWidth: 900,
-      targetHeight: 600,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      saveToPhotoAlbum: false,
-      allowEdit: true,
-      sourceType: 1
-    };
   }
 
-  take(then: (imageData: any) => void, err: (errors: any) => void): any {
-    this.camera.getPicture(this.options).then(then, err);
+  takeShot(): any {
+    this.camera.getPicture(this.options).then((imageData) => {
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.pictures.push(base64Image);
+      this._pictures$.next(this.pictures);
+    }, err => {
+      const stringError = JSON.stringify(err);
+      this.errors.push(stringError);
+      this._cameraError$.next(this.errors);
+    });
   }
 
 }
