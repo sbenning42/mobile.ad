8500876689d52ec70cf405962c78dccacb3273b1
Authentic Design <s.benning66@gmail.com>
2017-12-19 18:06:55 +0100

    finally stable

--- src/providers/camera/camera.ts
+++ src/providers/camera/camera.ts
@@ -18,11 +18,14 @@ export class CameraProvider {
     console.log('Hello CameraProvider Provider');
     this.options = <CameraOptions>{
       quality: 100,
+      targetWidth: 900,
+      targetHeight: 600,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
-      targetWidth: 600,
-      targetHeight: 600,
+      saveToPhotoAlbum: false,
+      allowEdit: true,
+      sourceType: 1
     };
   }
 
