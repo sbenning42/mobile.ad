5ab2e06bce2f623d51178c2a04210d87a7cf20ab
Authentic Design <s.benning66@gmail.com>
2017-12-20 14:21:36 +0100

    "test_autocomplete"

---
+++ syncGits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+glue="\""
+commitMessage=$glue$1$glue
+
+echo $commitMessage
+
+git add -A \
+&& git commit -m  $commitMessage \
+&& git push origin master \
+&& git push ionic master
